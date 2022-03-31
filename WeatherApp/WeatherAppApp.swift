794b8514ef52d0e88ca4122444b845012d91458d
chowdhury_shaheb <mhcsunny@gmail.com>
2022-04-01 03:34:06 +0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by MACBOOK Pro - WSIT on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
